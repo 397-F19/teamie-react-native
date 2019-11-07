a80f5c4e0301bb03dc872bab07ea2f3600514474
Shreyas Tallamraju <shreyastallamraju2020@u.northwestern.edu>
2019-11-07 17:23:58 -0600

    added react-native-paper and appbar example

--- teamie-react-native/App.js
+++ teamie-react-native/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import { Provider as PaperProvider, Appbar } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello Teamie</Text>
-    </View>
+      <View style={styles.container}>
+        <Appbar style={styles.bottom}>
+          <Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
+          <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+          <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+          <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
+        </Appbar>
+        <Text>Hello Teamie</Text>
+      </View>
   );
 }
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  )
+}
+
+AppRegistry.registerComponent('main', () => Main);
 
 const styles = StyleSheet.create({
   container: {
