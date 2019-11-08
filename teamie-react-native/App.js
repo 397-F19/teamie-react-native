c0dd04b78fa13938b9472e60d0d5d14a7ffdfeec
YifeiGong <yifeiaimeegong@hotmail.com>
2019-11-07 18:01:44 -0600

    card display

--- teamie-react-native/App.js
+++ teamie-react-native/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 import { Provider as PaperProvider, Appbar } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+
 
 const App = () => {
   return (
@@ -11,8 +13,19 @@ const App = () => {
           <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
           <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
     </Appbar>
-      <View style={styles.container}>
-        <Text>Hello Teamie</Text>
+      <View>
+      <Card>
+    <Card.Title title="Card Title" subtitle="Card Subtitle" left={(props) => <Avatar.Icon {...props} icon="folder" />} />
+    <Card.Content>
+      <Title>Card title</Title>
+      <Paragraph>Card content</Paragraph>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+  </Card>
       </View>
     </View>
   );
