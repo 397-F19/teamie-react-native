c830c43caaed9d0c511bdf4c37a9cffcdab5f6b5
taehunkim115 <35262234+taehunkim115@users.noreply.github.com>
2019-12-01 19:59:30 -0600

    paranthese problem fixed in constructor

--- teamie-react-native/App.js
+++ teamie-react-native/App.js
@@ -46,10 +46,7 @@ class App extends Component {
       selectedRestaurants: [],
       location: "",
     }
-    this.handleData = this.handleData.bind(this);
-    db.ref().on('value', this.handleData, e => console.log(e));
   }
-
   handleData() {
     let restaurantsList = [];
     db.ref().on('value', (snap) => {
@@ -181,9 +178,7 @@ class App extends Component {
   }
 
   sortRestaurants(sortType) {
-    console.log("sort triggered")
     if (sortType == "price_low_to_high") {
-      console.log("entered the right case")
       const filRest = this.state.filteredRestaurants.map(r => r);
       let sortedFilteredRestaurants = filRest.sort((a, b) => {
           if (a.price < b.price) {
